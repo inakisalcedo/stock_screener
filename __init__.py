281fc887fecfc46fcd94c2f6d9eb8b63600d6710
Iñaki Salcedo <67810965+inakisalcedo@users.noreply.github.com>
2026-09-09 13:08:08 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,1 @@
-# Archivo __init__.py del paquete "controlador".
-# Se mantiene vacío intencionalmente; su única función es indicarle a Python
-# que la carpeta "controlador" debe tratarse como un paquete importable,
-# permitiendo hacer `from controlador import control` desde app.py.
+
